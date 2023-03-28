8b7d629e7793b44ca7a926bc5e1ca0389a76c4ce
h42codes <hyungjikim42@gmail.com>
2023-03-28 00:12:06 -0400

    Implement search feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [breweries, setBreweries] = useState(null);
   // const [city, setCity] = useState("new_york");
+  const [filteredBreweries, setFilteredBreweries] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
 
   const callAPI = async () => {
     // const query = `https://api.openbrewerydb.org/v1/breweries?by_city=${city}`;
-    const query = `https://api.openbrewerydb.org/v1/breweries?per_page=50`;
+    const query = `https://api.openbrewerydb.org/v1/breweries`;
     const response = await axios.get(query);
     setBreweries(response.data);
   };
@@ -18,11 +24,41 @@ function App() {
     callAPI();
   }, []);
 
+  const handleSearch = () => {
+    const filteredBreweries = breweries.filter((brewery) => {
+      return brewery.name.toLowerCase().includes(searchInput.toLowerCase());
+    });
+    setBreweries(filteredBreweries);
+  };
+
+  const searchBreweries = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredBreweries = breweries.filter((brewery) => {
+        return brewery.name.toLowerCase().includes(searchValue.toLowerCase());
+      });
+      setFilteredBreweries(filteredBreweries);
+    } else {
+      setFilteredBreweries(breweries);
+    }
+  };
+
   return (
     <div className="App">
       {/* <h1>Breweries in {city}</h1> */}
       <h1>Breweries</h1>
-      {breweries && <BreweryList breweries={breweries} />}
+      <input
+        type="text"
+        value={searchInput}
+        onChange={(inputString) => searchBreweries(inputString.target.value)}
+      />
+      {/* <button onClick={handleSearch}>Search</button> */}
+      {/* {breweries && <BreweryList breweries={breweries} />} */}
+      {searchInput.length > 0 ? (
+        <BreweryList breweries={filteredBreweries} />
+      ) : (
+        breweries && <BreweryList breweries={breweries} />
+      )}
     </div>
   );
 }
