7280c014ad66b62d1bd95aeefcbb01f7fac0ea00
h42codes <hyungjikim42@gmail.com>
2023-03-28 00:44:21 -0400

    Add stats

--- src/components/BreweryList.jsx
+++ src/components/BreweryList.jsx
@@ -2,17 +2,41 @@ import BreweryCard from "./BreweryCard";
 import "./BreweryList.css";
 
 const BreweryList = ({ breweries }) => {
+  const mostFrequentType = () => {
+    let types = {};
+    breweries.forEach((b) => {
+      if (types[b.brewery_type]) {
+        types[b.brewery_type] += 1;
+      } else {
+        types[b.brewery_type] = 1;
+      }
+    });
+    let max = 0;
+    let maxType = "";
+    for (let type in types) {
+      if (types[type] > max) {
+        max = types[type];
+        maxType = type;
+      }
+    }
+    return maxType;
+  };
+
   return (
     <div className="BreweryList">
       <div className="brewery-stats">
         <p>Found {breweries && breweries.length} breweries</p>
         <p>
-          {breweries.filter((b) => b.brewery_type == "closed").length} closed
-          breweries
+          {breweries.filter((b) => b.brewery_type == "closed").length} are
+          closed
         </p>
-
-        <p>
+        {/* <p>
           {breweries.filter((b) => b.brewery_type == "micro").length} micro
+          breweries
+        </p> */}
+        <p>
+          {mostFrequentType()} brewries are the most common type (
+          {breweries.filter((b) => b.brewery_type == "micro").length} total) of
           breweries
         </p>
       </div>
