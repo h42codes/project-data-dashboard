555ea689561a8f528f988521c9d8c66e5133f01f
h42codes <hyungjikim42@gmail.com>
2023-04-04 00:05:58 -0400

    Implement chart toggle feature

--- src/components/BreweryList.jsx
+++ src/components/BreweryList.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BreweryCard from "./BreweryCard";
 import "./BreweryList.css";
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, PieChart, Pie } from "recharts";
 
 const BreweryList = ({ breweries }) => {
+  const [showBarChart, setShowBarChart] = useState(true);
+
   const breweryCounts = {};
   breweries.forEach((brewery) => {
     const breweryType = brewery.brewery_type;
@@ -60,12 +62,35 @@ const BreweryList = ({ breweries }) => {
         </p>
       </div>
       <div className="brewery-chart">
-        <BarChart width={700} height={300} data={breweryTypeCounts}>
-          <Bar dataKey="count" fill="#85586f" />
-          <XAxis dataKey="type" />
-          <YAxis />
-          <Tooltip />
-        </BarChart>
+        <button
+          onClick={() => {
+            setShowBarChart(!showBarChart);
+          }}
+        >
+          {showBarChart ? "View Pie Chart Instead" : "View Bar Chart Instead"}
+        </button>
+        {showBarChart ? (
+          <BarChart width={700} height={300} data={breweryTypeCounts}>
+            <Bar dataKey="count" fill="#85586f" />
+            <XAxis dataKey="type" />
+            <YAxis />
+            <Tooltip />
+          </BarChart>
+        ) : (
+          <PieChart width={700} height={300}>
+            <Pie
+              data={breweryTypeCounts}
+              dataKey="count"
+              nameKey="type"
+              cx="50%"
+              cy="50%"
+              outerRadius={100}
+              fill="#85586f"
+              label
+            />
+            <Tooltip />
+          </PieChart>
+        )}
       </div>
       <div className="brewery-container">
         {breweries.map((brewery) => (
